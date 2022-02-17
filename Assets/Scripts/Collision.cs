8d04e60de5f6ee369305cb0d19b2f0c2daa51b78
Keeley Riley <riley291@morrisville.edu>
2022-02-16 22:44:05 -0500

    Add Assets to Project

--- Assets/Scripts/Collision.cs
+++ Assets/Scripts/Collision.cs
@@ -4,7 +4,12 @@ using UnityEngine;
 
 public class Collision : MonoBehaviour
 {
-    void OnCollisionEnter2D(Collision2D other) {
+    void OnCollisionEnter2D(Collision2D other) 
+    {
        Debug.Log("I have squirrels in my pants!"); 
+    }
+    void OnTriggerEnter2D(Collider2D other) 
+    {
+        Debug.Log("Butter Nut!");
     }
 }
